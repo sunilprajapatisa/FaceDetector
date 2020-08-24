6a41393d9af6bce50fc3e90f748af6307821ceb1
Sunil Prajapati <sunil.prajapati@solutionanalysts.com>
2020-08-24 16:05:02 +0530

    [config]:- initial config files

---
+++ FaceDetector/FaceDetector.h
@@ -0,0 +1,19 @@
+//
+//  FaceDetector.h
+//  FaceDetector
+//
+//  Created by Sunil Prajapati on 24/08/20.
+//  Copyright © 2020 Sunil Prajapati. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FaceDetector.
+FOUNDATION_EXPORT double FaceDetectorVersionNumber;
+
+//! Project version string for FaceDetector.
+FOUNDATION_EXPORT const unsigned char FaceDetectorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FaceDetector/PublicHeader.h>
+
+
